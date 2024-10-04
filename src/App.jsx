97fef3e7a463fd0195ab49b5929073d5bb1e78eb
Ines Ch <ineschabla60@gmail.com>
2024-10-04 19:02:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Let build a fullstack MERN TikTok Clone</h1>
+    </>
+  );
+}
+
+export default App;
